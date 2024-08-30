4ab777366d71933dc0b6d35f850bc807b8898ed2
b_e_sa <joaospereira963@gmail.com>
2024-08-30 16:15:05 -0300

    add specific box shadow and border radius to mui theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,19 @@
 "use client";
-import { createTheme } from "@mui/material/styles";
+import { createTheme, Shadows } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
     primary: {
       main: "#0062FF",
     },
+  },
+  shadows: [
+    "none",
+    "2px 2px 4px 3px rgba(0, 0, 0, 0.25)",
+    ...Array(23).fill("none")
+  ] as Shadows,
+  shape: {
+    borderRadius: 25,
   },
 });
 
