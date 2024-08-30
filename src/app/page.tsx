6245a3c02d184712fc78ea009dc5d4702c9afe53
b_e_sa <joaospereira963@gmail.com>
2024-08-30 16:16:07 -0300

    pass sx styles to form component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import CreateUrlForm from "../components/CreateUrlForm";
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center w-screen">
-      <CreateUrlForm />
+      <CreateUrlForm sx={{ marginBottom: 9, marginTop: 6 }} />
       <TopUrls />
     </main>
   );
