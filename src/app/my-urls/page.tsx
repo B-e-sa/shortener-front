f3db500d319c3fa04338c17e5dac17eac99c769f
b_e_sa <joaospereira963@gmail.com>
2024-08-30 21:28:04 -0300

    add my urls table wrapper

--- src/app/my-urls/page.tsx
+++ src/app/my-urls/page.tsx
@@ -6,6 +6,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import { Box, Button } from "@mui/material";
 
 function createData(
   title: string,
@@ -51,41 +52,61 @@ const rows = [
 
 export default function Page() {
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">Title</TableCell>
-            <TableCell align="left">URL</TableCell>
-            <TableCell align="left">Short URL</TableCell>
-            <TableCell align="left">Visits</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.title}>
-              <TableCell width={200} component="th" scope="row">
-                {row.title}
-              </TableCell>
-              <TableCell align="left">{row.url}</TableCell>
-              <TableCell align="left">{row.shortUrl}</TableCell>
-              <TableCell align="left">{row.visits}</TableCell>
-              <TableCell sx={{ display: "flex" }} align="center">
-                <Typography
-                  sx={{ marginRight: 7 }}
-                  variant="body1"
-                  color="initial"
-                >
-                  Delete
-                </Typography>
-                <Typography variant="body1" color="initial">
-                  Copy
-                </Typography>
-              </TableCell>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <TableContainer
+        sx={{
+          width: 850,
+          height: 600,
+        }}
+        component={Paper}
+      >
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Title</TableCell>
+              <TableCell align="left">URL</TableCell>
+              <TableCell align="left">Short URL</TableCell>
+              <TableCell align="left">Visits</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.title}>
+                <TableCell width={200} component="th" scope="row">
+                  {row.title}
+                </TableCell>
+                <TableCell align="left">{row.url}</TableCell>
+                <TableCell align="left">{row.shortUrl}</TableCell>
+                <TableCell align="left">{row.visits}</TableCell>
+                <TableCell width={70} align="center">
+                  <Button
+                    sx={{ borderRadius: 0 }}
+                    variant="contained"
+                    color="error"
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+                <TableCell width={70} align="center">
+                  <Button
+                    sx={{ bgcolor: "primary.main", borderRadius: 0 }}
+                    variant="contained"
+                  >
+                    Copy
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
