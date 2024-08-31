e2318b6ed22c98a5b7c1e8da663aee8f08ea6309
b_e_sa <joaospereira963@gmail.com>
2024-08-30 21:24:40 -0300

    add breakpoints

--- src/theme.ts
+++ src/theme.ts
@@ -10,10 +10,19 @@ const theme = createTheme({
   shadows: [
     "none",
     "2px 2px 4px 3px rgba(0, 0, 0, 0.25)",
-    ...Array(23).fill("none")
+    ...Array(23).fill("none"),
   ] as Shadows,
   shape: {
     borderRadius: 25,
+  },
+  breakpoints: {
+    values: {
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xs: 1536,
+    },
   },
 });
 
