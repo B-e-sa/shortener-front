b153fed9b87a3636201f0282fedce7ea47b93ed1
b_e_sa <joaospereira963@gmail.com>
2024-08-30 21:30:32 -0300

    switch from tailwind styling to mui styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import TopUrls from "@/components/TopUrls";
 import CreateUrlForm from "../components/CreateUrlForm";
+import { Box } from "@mui/material";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center w-screen">
-      <CreateUrlForm sx={{ marginBottom: 9, marginTop: 6 }} />
-      <TopUrls />
+    <main style={{ display: "flex", justifyContent: "center" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "Center",
+          width: 650
+        }}
+      >
+        <CreateUrlForm sx={{ marginBottom: 9, marginTop: 6 }} />
+        <TopUrls sx={{ marginBottom: 6 }} />
+      </Box>
     </main>
   );
 }
