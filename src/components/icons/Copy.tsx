4aa3a4c1454ec2d76e0899d5e8c3458ba7396041
b_e_sa <joaospereira963@gmail.com>
2024-09-04 16:31:08 -0300

    adjust copy icon fillings

--- src/components/icons/Copy.tsx
+++ src/components/icons/Copy.tsx
@@ -6,18 +6,19 @@ export default function CopyIcon(props: ComponentProps<"svg">) {
       width="40px"
       height="40px"
       viewBox="0 0 24 24"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
         d="M10 13a3 3 0 013-3h5a3 3 0 013 3v5a3 3 0 01-3 3h-5a3 3 0 01-3-3v-5z"
-        stroke="#000"
+        stroke={props.fill}
+        fill="none"
         strokeWidth={2}
       />
       <path
         d="M14 6.208V6a3 3 0 00-3-3H6a3 3 0 00-3 3v5a3 3 0 003 3h.208"
-        stroke="#000"
+        stroke={props.fill}
+        fill="none"
         strokeWidth={2}
         strokeLinecap="round"
       />
